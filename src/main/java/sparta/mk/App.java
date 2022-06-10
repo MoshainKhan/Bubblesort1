459cc3cc9e475369b18f15919a3ea23b5ec82156
Mohsin <mkhan@spartaglobal.com>
2022-06-10 17:42:21 +0100

    New Bubblesort

--- src/main/java/sparta/mk/App.java
+++ src/main/java/sparta/mk/App.java
@@ -1,31 +1,13 @@
 package sparta.mk;
 
-
+import java.util.random.RandomGenerator;
+import java.util.Scanner;
 
 import java.util.Arrays;
 class App {
 
     // perform the bubble sort
-    static void bubbleSort(int array[]) {
-        int size = array.length;
 
-        // loop to access each array element
-        for (int i = 0; i < size - 1; i++)
-
-            // loop to compare array elements
-            for (int max = 0; max < size - i - 1; max++)
-
-                // compare two adjacent elements
-                // change > to < to sort in descending order
-                if (array[max] > array[max + 1]) {
-
-                    // swapping occurs if elements
-                    // are not in the intended order
-                    int temp = array[max];
-                    array[max] = array[max + 1];
-                    array[max + 1] = temp;
-                }
-    }
 
     public static void main(String args[]) {
 
